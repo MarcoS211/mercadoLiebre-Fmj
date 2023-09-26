95442be28adf44bcda9b4cf431d190ec33b410e2
Ferrero Marcos <ferrero.marcos.92@gmail.com>
2023-09-26 14:24:33 -0300

    Update--

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
-
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
 
 const port = process.env.PORT || 3030 ;
 app.listen(port, () => {
